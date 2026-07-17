3faa3313b95287e384aa123d0ab3fd3558807349
Arto Bendiken <arto@bendiken.net>
2026-07-17 03:07:23 -0700

    Render code blocks for files recognized by Linguist.

--- rust/src/engine/liquid/file.rs
+++ rust/src/engine/liquid/file.rs
@@ -3,11 +3,13 @@
 use crate::RootedPath;
 use alloc::{
     borrow::{Cow, ToOwned},
+    format,
     string::{String, ToString},
     vec,
     vec::Vec,
 };
 use camino::Utf8PathBuf;
+use linguist::{detect_language_by_extension, detect_language_by_filename};
 use liquid::{
     Template,
     partials::{EagerCompiler, LazyCompiler, PartialSource},
@@ -35,14 +37,51 @@ impl FileSource {
         if !path.try_exists()? {
             return Err(ErrorKind::NotFound.into());
         }
-        let mut result = match path.extension() {
-            Some("liquid") | Some("md") => read_to_string(path)?,
-            Some("json") => todo!(),                            // TODO
-            Some("csv") | Some("tsv") => todo!(),               // TODO
-            _ => return Err(ErrorKind::InvalidFilename.into()), // Rust 1.87+
-        };
-        result.truncate(result.trim_ascii_end().len());
-        Ok(result)
+
+        let languages = detect_language_by_filename(&path).unwrap(); // FIXME
+        if let Some(language) = languages.first() {
+            let code = read_to_string(path)?;
+            return Ok(self.format_code(language.name, code));
+        }
+
+        match path.extension() {
+            Some("liquid") | Some("md") => {
+                let mut result = read_to_string(path)?;
+                result.truncate(result.trim_ascii_end().len());
+                Ok(result)
+            },
+            Some("csv") | Some("tsv") => todo!(), // TODO
+            Some("rs") => {
+                let code = read_to_string(path)?;
+                Ok(self.format_code("rust", code))
+            },
+            Some("json") | Some(_) => {
+                let languages = detect_language_by_extension(&path).unwrap(); // FIXME
+                let Some(language) = languages.first() else {
+                    return Err(ErrorKind::InvalidFilename.into());
+                };
+                let code = read_to_string(path)?;
+                let tag: String = language
+                    .name
+                    .chars()
+                    .map(|c| match c {
+                        ' ' => '-',
+                        c => c.to_ascii_lowercase(),
+                    })
+                    .collect();
+                Ok(self.format_code(tag, code))
+            },
+            None => return Err(ErrorKind::InvalidFilename.into()),
+        }
+    }
+
+    fn format_code(&self, tag: impl AsRef<str>, code: impl Into<String>) -> String {
+        let code = code.into();
+        format!(
+            "```{tag}\n{code}\n```",
+            tag = tag.as_ref(),
+            code = code.trim_ascii_end()
+        )
     }
 
     fn join(&self, name: &str) -> Utf8PathBuf {
