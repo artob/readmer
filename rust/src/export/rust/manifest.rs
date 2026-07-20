6f228aa251c5373cee455f4566252e2a6717237e
Arto Bendiken <arto@bendiken.net>
2026-07-19 22:23:26 -0700

    Use the Rust edition if version absent.

--- rust/src/export/rust/manifest.rs
+++ rust/src/export/rust/manifest.rs
@@ -1,3 +1,3 @@
 // This is free and unencumbered software released into the public domain.
 
-pub use cargo_toml::{Manifest, Value};
+pub use cargo_toml::{Edition, Manifest, Value};
--- rust/src/model/package/rust.rs
+++ rust/src/model/package/rust.rs
@@ -4,17 +4,27 @@ impl TryFrom<export::rust::Manifest> for Package {
     type Error = export::rust::LoadManifestError;
 
     fn try_from(input: export::rust::Manifest) -> Result<Self, Self::Error> {
-        use export::rust::Value;
+        use export::rust::{Edition, Value};
         assert!(!input.needs_workspace_inheritance());
         let package = input.package.unwrap();
-        let rust_version = package.rust_version.map(|x| x.unwrap());
+        let rust_edition = package.edition.unwrap();
+        let rust_version = package.rust_version.map(|x| x.unwrap()).unwrap_or_else(|| {
+            match rust_edition {
+                Edition::E2024 => "2024",
+                Edition::E2021 => "2021",
+                Edition::E2018 => "2018",
+                Edition::E2015 => "2015",
+                _ => "2015", // Cargo assumes 2015 if absent
+            }
+            .into()
+        });
         Ok(Self {
             language: Language {
                 name: "rust".into(),
                 label: "Rust".into(),
                 extensions: vec![".rs".into(), ".rs.in".into()],
-                version: rust_version.clone(),
-                minimum_version: rust_version,
+                version: Some(rust_version.clone()),
+                minimum_version: Some(rust_version),
                 ..Default::default()
             },
             languages: vec![],
